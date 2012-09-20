9230ee6490d2b80e959cdb6f1af6e2bb56830d85
rilian <dmitriyis@gmail.com>
2012-09-20 19:47:17 +0300

    metadata

--- kitchen/cookbooks/additional_packages/metadata.rb
+++ kitchen/cookbooks/additional_packages/metadata.rb
@@ -4,3 +4,5 @@ license          "All rights reserved"
 description      "Installs/Configures additional_packages"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
+
+recipe "additional_packages", 'Process list ...'
